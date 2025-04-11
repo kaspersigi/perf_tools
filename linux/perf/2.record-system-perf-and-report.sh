6b4d998f1f5c0ab0c29c4261ae6cfeb8de5d2643
kaspersigi <kaspersigi@outlook.com>
2025-04-11 22:08:13 +0800

    update linux perf

--- linux/perf/2.record-system-perf-and-report.sh
+++ linux/perf/2.record-system-perf-and-report.sh
@@ -1,17 +1,13 @@
 DRAW=./FlameGraph
 PROG=main
 
-perf record -e instructions -a --duration 10 -c 100000 -g -o perf.data
+perf record -F 999 -ag -- sleep 10
+perf script > out.perf-txt
 
 # 年月日_时分秒
 timer=$(date +%Y%m%d_%H%M%S)
 echo $timer.perf-data
 mv perf.data $timer.perf-data
-
-python3 $FOLD -i $timer.perf-data > $timer.perf-folded
-
-python3 $FOX -i $timer.perf-data > $timer.json.gz
+mv out.perf-txt $timer.perf-txt
 
 perl $DRAW/flamegraph.pl --title "$timer.perf-folded" $timer.perf-folded > $timer.svg
-
-rm $timer.perf-folded
