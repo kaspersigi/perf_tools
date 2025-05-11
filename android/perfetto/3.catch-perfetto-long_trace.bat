53b26ad119e16de8365b609d7f437af213a24fea
kaspersigi <kaspersigi@outlook.com>
2025-05-11 14:05:52 +0800

    long trace use decompress version

--- android/perfetto/3.catch-perfetto-long_trace.bat
+++ android/perfetto/3.catch-perfetto-long_trace.bat
@@ -4,8 +4,8 @@ adb devices
 adb root
 adb remount
 
-adb shell -t /data/local/tmp/tracebox --txt -c /data/local/tmp/long_trace_config.pbtxt -o /data/local/tmp/trace.compressed-perfetto-trace
-adb pull /data/local/tmp/trace.compressed-perfetto-trace
+adb shell -t /data/local/tmp/tracebox --txt -c /data/local/tmp/long_trace_config.pbtxt -o /data/local/tmp/trace.decompressed-perfetto-trace
+adb pull /data/local/tmp/trace.decompressed-perfetto-trace
 
 ::时间不是两位补0
 if "%date:~5,2%" lss "10" (set mm=0%date:~6,1%) else (set mm=%date:~5,2%)
@@ -15,8 +15,8 @@ if "%time:~3,2%" lss "10" (set nn=0%time:~4,1%) else (set nn=%time:~3,2%)
 
 :: 年月日_时分秒
 set timer=%date:~0,4%%mm%%dd%_%hh%%nn%%time:~6,2%
-echo %timer%.compressed-perfetto-trace
-rename trace.compressed-perfetto-trace %timer%.compressed-perfetto-trace
+echo %timer%.decompressed-perfetto-trace
+rename trace.decompressed-perfetto-trace %timer%.decompressed-perfetto-trace
 
 :: py -3 ./analys/sql/analys_common.py -f %timer%.decompressed-perfetto-trace >> log.tsv
 :: py -3 ./analys/perfetto/analys.py -f %timer%.decompressed-perfetto-trace
--- android/perfetto/3.catch-perfetto-long_trace.sh
+++ android/perfetto/3.catch-perfetto-long_trace.sh
@@ -21,13 +21,13 @@ $ADB devices
 $ADB root
 $ADB remount
 
-$ADB shell -t /data/local/tmp/tracebox --txt -c /data/local/tmp/long_trace_config.pbtxt -o /data/local/tmp/trace.compressed-perfetto-trace
-$ADB pull /data/local/tmp/trace.compressed-perfetto-trace
+$ADB shell -t /data/local/tmp/tracebox --txt -c /data/local/tmp/long_trace_config.pbtxt -o /data/local/tmp/trace.decompressed-perfetto-trace
+$ADB pull /data/local/tmp/trace.decompressed-perfetto-trace
 
 # 年月日_时分秒
 timer=$(date +%Y%m%d_%H%M%S)
-echo $timer.compressed-perfetto-trace
-mv trace.compressed-perfetto-trace $timer.compressed-perfetto-trace
+echo $timer.decompressed-perfetto-trace
+mv trace.decompressed-perfetto-trace $timer.decompressed-perfetto-trace
 
 # python3 ./analys/sql/analys_common.py -f $timer.decompressed-perfetto-trace >> log.tsv
 # python3 ./analys/perfetto/analys.py -f $timer.decompressed-perfetto-trace
